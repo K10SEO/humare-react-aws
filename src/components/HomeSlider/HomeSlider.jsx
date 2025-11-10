8ff8bfa5f796e487a9bce2d34b0a804bbae3a798
K10SEO <limk10tj@gmail.com>
2025-11-11 02:05:20 +0900

    add: slider text

--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -4,8 +4,12 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styled from 'styled-components'
 
+// 데스크톱 이미지
 import slider1 from '../../assets/img/slider/slider1.webp';
 import slider2 from '../../assets/img/slider/slider2.webp';
+
+// slider2의 모바일 이미지
+import slider2Mobile from '../../assets/img/slider/slider_img_mobile.webp';
 import SliderImgContainer from './SliderImgContainer/SliderImgContainer';
 
 
@@ -38,33 +42,27 @@ export default function HomeSlider(){
     return (
         <SliderContainer reHeight={reHeight}>
             <Slider {...settings}>
-                {/* 사진 1: 왼쪽 정렬, 상단에서 5rem 아래 */}
+                {/* slider1: 데스크톱/모바일 동일 이미지 */}
                 <SliderImgContainer 
-                  src={slider1} 
+                  src={slider1}
                   alt={'아파트사진'}
                   subtitle="PREMIUM"
-                  title="• 푸르지오의 검증된 가치,<br/>• 주거의 특화된 단지와공간설계<br/>• 도시를 대표하는 인프라"
+                  title="• 푸르지오의 검증된 가치,<br/>• 주거의 특화된 단지와공간설계,<br/>• 도시를 대표하는 인프라"
                   showPhone={false}
-                  textAlign="left"
-                  topMargin="40rem"
-                  leftPadding="10rem"
-                  rightPadding="10rem"
+                  isBuildingSlide={true}
                 />
-
-                {/* 사진 2: 왼쪽 정렬, 하단에서 3rem 위 */}
+                
+                {/* slider2: 모바일은 다른 이미지 */}
                 <SliderImgContainer 
-                  src={slider2} 
+                  src={slider2}
+                  srcMobile={slider2Mobile}  // ⭐ 모바일 전용 이미지
                   alt={'아파트사진'}
                   subtitle="신길 AK PRUGIO"
                   title="삶이 남다른 서울의 중심,<br/>푸르지오의 높은 자부심"
                   phoneLabel="분양문의 대표 상담번호"
                   phoneNumber="1661-3822"
                   showPhone={true}
-                  textAlign="left"
-                  bottomMargin="-5rem"
-                  topMargin="15rem"
-                  leftPadding="15rem"
-                  rightPadding="15rem"
+                  isBuildingSlide={false}
                 />
             </Slider>
         </SliderContainer>
--- src/components/HomeSlider/SliderImgContainer/SliderImgContainer.jsx
+++ src/components/HomeSlider/SliderImgContainer/SliderImgContainer.jsx
@@ -1,173 +1,160 @@
-import styled from 'styled-components'
+import { useState, useEffect } from 'react'
+import styled, { keyframes } from 'styled-components'
 
 export default function SliderImgContainer({
-    src, 
+    src,
+    srcMobile,  // ⭐ 모바일 이미지 추가
     alt, 
     subtitle, 
     title, 
     phoneLabel, 
     phoneNumber, 
     showPhone,
-    textAlign = 'left',  // ⭐ 텍스트 정렬 (left, center, right)
-    topMargin = '0',     // ⭐ 상단 텍스트 위쪽 여백
-    bottomMargin = '0',  // ⭐ 하단 텍스트 아래쪽 여백
-    leftPadding = '15rem', // ⭐ 왼쪽 패딩
-    rightPadding = '15rem' // ⭐ 오른쪽 패딩
+    isBuildingSlide = false
 }){
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 767);
+        };
+        
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    // ⭐ 모바일이고 srcMobile이 있으면 srcMobile 사용, 아니면 src 사용
+    const imageSrc = isMobile && srcMobile ? srcMobile : src;
+
     return (
-        <ImgContainer>
-            <SlideImg src={src} alt={alt}/>
-            <TextOverlay leftPadding={leftPadding} rightPadding={rightPadding}>
-                <TopText textAlign={textAlign} topMargin={topMargin}>
-                    <SubTitle>{subtitle}</SubTitle>
-                    <MainTitle dangerouslySetInnerHTML={{__html: title}} />
-                </TopText>
-                {showPhone && (
-                    <BottomText textAlign={textAlign} bottomMargin={bottomMargin}>
-                        <PhoneLabel>{phoneLabel}</PhoneLabel>
-                        <PhoneNumber>{phoneNumber}</PhoneNumber>
-                    </BottomText>
-                )}
-            </TextOverlay>
-        </ImgContainer>
+        <SlideContainer>
+            <SliderBox>
+                <SlideImg src={imageSrc} alt={alt} />
+            </SliderBox>
+            
+            <TextWrap isBuildingSlide={isBuildingSlide}>
+                <SubTitle>{subtitle}</SubTitle>
+                <MainTitle dangerouslySetInnerHTML={{__html: title}} />
+            </TextWrap>
+            
+            {showPhone && (
+                <SlideTell>
+                    {phoneLabel}<br/>
+                    <TellNumber>{phoneNumber}</TellNumber>
+                </SlideTell>
+            )}
+        </SlideContainer>
     )
 }
 
-const ImgContainer = styled.div`
-    width: 100%;
+// 전화번호 색상 애니메이션
+const tellColor = keyframes`
+    25% { color: yellow; }
+    50% { color: orange; }
+    75% { color: red; }
+`
+
+const SlideContainer = styled.div`
     position: relative;
-`
-
-const SlideImg = styled.img`
-    width: 100%;
-    height: 82rem;
-    object-fit: fill;
-    display: block;
-
-    @media (max-width: 1024px) {
-        height: 52rem;
-    }
-
-    @media (max-width: 768px) {
-        height: 32rem;
-    }
-
-    @media (max-width: 480px) { 
-        height: 22rem;
+    overflow: hidden;
+    padding-top: 43%;
+    max-width: 1920px;
+    
+    @media (max-width: 767px) {
+        padding-top: 120%;
     }
 `
 
-const TextOverlay = styled.div`
+const SliderBox = styled.div`
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 8rem ${props => props.rightPadding} 8rem ${props => props.leftPadding};
-    color: white;
-
-    @media (max-width: 1024px) {
-        padding: 5rem 10rem;
-    }
-
-    @media (max-width: 768px) {
-        padding: 3rem 5rem;
-    }
-
-    @media (max-width: 480px) {
-        padding: 2rem 3rem;
-    }
+    z-index: 1;
 `
 
-const TopText = styled.div`
-    text-align: ${props => props.textAlign};
-    margin-top: ${props => props.topMargin};
+const SlideImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
 `
 
-const SubTitle = styled.h3`
-    font-size: 2.5rem;
-    font-weight: 400;
-    margin-bottom: 2rem;
+const TextWrap = styled.div`
+    display: block;
+    width: 40%;
+    color: #fff;
+    position: absolute;
+    left: 10%;
+    top: ${props => props.isBuildingSlide ? '60%' : '30%'};
+    z-index: 10;
     
-    @media (max-width: 1024px) {
-        font-size: 2rem;
+    @media (max-width: 767px) {
+        width: 100%;
+        background-color: rgb(27, 81, 83);
+        left: 0;
+        top: ${props => props.isBuildingSlide ? '73%' : '0'};
+        height: ${props => props.isBuildingSlide ? 'auto' : '27%'};
+        padding: ${props => props.isBuildingSlide ? '2% 0 4% 12%' : '5% 16% 0'};
     }
+`
 
-    @media (max-width: 768px) {
-        font-size: 1.5rem;
-        margin-bottom: 1rem;
-    }
-
-    @media (max-width: 480px) {
-        font-size: 1.2rem;
+const SubTitle = styled.h5`
+    line-height: 140%;
+    font-size: 1.4vw;
+    letter-spacing: 1px;
+    margin: 0 0 0.7vw 0;
+    color: #fff;
+    font-weight: 900;
+    text-shadow: 3px 3px 4px black;
+    
+    @media (max-width: 767px) {
+        font-size: 2.4vw;
+        margin: 0 0 3vw 0;
     }
 `
 
 const MainTitle = styled.h1`
-    font-size: 5rem;
-    font-weight: 700;
-    line-height: 1.4;
-    
-    @media (max-width: 1024px) {
-        font-size: 3.5rem;
-    }
-
-    @media (max-width: 768px) {
-        font-size: 2.5rem;
-    }
-
-    @media (max-width: 480px) {
-        font-size: 1.8rem;
+    font-size: 2.2vw;
+    line-height: 140%;
+    /* padding: 2rem; */
+    @media (max-width: 767px) {
+        font-size: 5.2vw;
     }
 `
 
-const BottomText = styled.div`
-    text-align: ${props => props.textAlign};
-    margin-bottom: ${props => props.bottomMargin};
-`
-
-const PhoneLabel = styled.p`
-    font-size: 4rem;
-    font-weight: 900;
-    margin-bottom: 1.5rem;
-    margin-left: 1.5rem;
+const SlideTell = styled.div`
+    position: absolute;
+    width: 47%;
+    height: 20%;
+    left: 0%;
+    top: 80%;
+    font-family: 'TmonMonsori';
+    font-size: 2vw;
     color: var(--main-green);
-    
-    @media (max-width: 1024px) {
-        font-size: 1.6rem;
-    }
+    font-weight: 900;
+    text-align: center;
+    line-height: 1.2;
+    z-index: 10;
+    /* background-color: rgba(27, 81, 83, 0.8); */
+    padding: 1rem;
 
-    @media (max-width: 768px) {
-        font-size: 1.3rem;
-        margin-bottom: 1rem;
-    }
-
-    @media (max-width: 480px) {
-        font-size: 1rem;
+    @media (max-width: 767px) {
+        background-color: #1b5153;
+        border-radius: 10% 10% 10% 10%;
+        letter-spacing: 1px;
+        padding: 2%;
+        left: -2%;
+        top: 90%;
+        color: #fff;
+        font-weight: 500;
     }
 `
 
-const PhoneNumber = styled.h2`
-    font-size: 7rem;
-    font-weight: 900;
-    color: #ffeb3b;
-    text-shadow: 4px 4px 8px rgba(0,0,0,0.6);
-    letter-spacing: 0.5rem;
-    
-    @media (max-width: 1024px) {
-        font-size: 5rem;
-    }
-
-    @media (max-width: 768px) {
-        font-size: 3.5rem;
-        letter-spacing: 0.3rem;
-    }
-
-    @media (max-width: 480px) {
-        font-size: 2.5rem;
-        letter-spacing: 0.2rem;
-    }
+const TellNumber = styled.span`
+    color: yellow;
+    font-size: 5vw;
+    animation: ${tellColor} 5s infinite;
+    text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
+    display: block;
 `
